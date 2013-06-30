800671b6c1f43e51a78e1d54b327364963551fed
Luke Frisken <l.frisken@gmail.com>
2013-06-30 21:32:21 +1000

    Added gpl license

--- Source2Pdf.py
+++ Source2Pdf.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python
+
+"""
+Source2Pdf.py
+This file is part of Source2PDF
+
+Copyright (C) 2013 - Luke Frisken
+
+Source2PDF is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+Source2PDF is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Source2PDF. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
 import cStringIO as StringIO
 import os, os.path, sys, datetime, pwd, re
 import argparse
