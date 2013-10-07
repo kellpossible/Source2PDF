03df88ce84355fb99af9705b4d727b06796ceda5
l.frisken@gmail.com <l.frisken@gmail.com>
2013-10-08 01:17:12 +1100

    Updated build_pdf.sh to include the github css file

--- build_pdf.sh
+++ build_pdf.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-python2 Source2Pdf.py --ext py sh --exclude .*AssemblyInfo.* .*gtk-gui.* \
+#currently hidden folders with fullstop prefix are automatically excluded
+#anyway, but this is just an example of how you might exclude it using regex
+python2 Source2Pdf.py --ext py sh css --exclude .*[\\.]git.* \
 -o Source2Pdf.pdf --project-name Source2Pdf --style default --user-name kellpossible \
 --line-numbers
