badbbdc042528de063c8bbec3bbf58a2ca299f7c
l.frisken@gmail.com <l.frisken@gmail.com>
2013-10-08 00:16:47 +1100

    added an example built compiled pdf of this project for people to see the output they can expect

--- build_pdf.sh
+++ build_pdf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python2 Source2Pdf.py --ext py sh --exclude .*AssemblyInfo.* .*gtk-gui.* \
--o Source2Pdf.pdf --project-name Source2Pdf --style default \
+-o Source2Pdf.pdf --project-name Source2Pdf --style default --user-name kellpossible \
 --line-numbers
